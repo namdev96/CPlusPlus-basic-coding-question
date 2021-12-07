32e46671350249d1029039ba1bcf6c6f5a9d4738
priyanka namdev <priyanka@intozi.io>
2021-12-07 23:21:01 +0530

    basic c++ codes

---
+++ print-number.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num;
+cout <<"Enter Number";
+cin >> num;
+cout << "You Entered" <<number;
+
+}
+
+
---
+++ sum-of-two-numbers.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num1,num2,sum=0;
+cout <<"Enter Two Numbers";
+cin >> num1>>num2;
+
+sum =num1+num2;
+cout << "SUM= " << sum;
+}
